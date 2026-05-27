519cb00a4cf2c75d36df79fac9be185284cc2599
Fabiola <fabiola.aurelien@gmail.com>
2026-05-27 16:52:40 -0400

    'Defined the routes for the application

--- RecipeDiscoveryApp/src/App.tsx
+++ RecipeDiscoveryApp/src/App.tsx
@@ -1,14 +1,27 @@
+import { Routes, Route } from "react-router-dom";
 
-import './App.css'
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import RecipeDetail from "./pages/RecipeDetail";
+import Favorites from "./pages/Favorites";
+import SearchResults from "./pages/SearchResults";
 
 function App() {
-
-
   return (
-    <>
-     <h1>Welcome to React with TypeScript!</h1>
-    </>
-  )
+
+    // Define the routes for the application
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+      <Route path="/category/:categoryName" element={<Category />}/>
+
+      <Route path="/recipe/:recipeId" element={<RecipeDetail />}/>
+
+      <Route path="/favorites" element={<Favorites />}/>
+
+      <Route path="/search" element={<SearchResults />}/>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
