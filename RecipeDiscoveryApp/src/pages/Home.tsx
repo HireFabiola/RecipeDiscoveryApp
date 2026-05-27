d96c30a9ebf65e452717c9bb91471175d862b517
Fabiola <fabiola.aurelien@gmail.com>
2026-05-27 18:20:36 -0400

    Added simple code for testing useEffect hook

--- RecipeDiscoveryApp/src/pages/Home.tsx
+++ RecipeDiscoveryApp/src/pages/Home.tsx
@@ -1,5 +1,30 @@
+import useFetch from "../hooks/useFetch";
+
+const categoriesUrl =
+  "https://www.themealdb.com/api/json/v1/1/categories.php";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  const { data, loading, error } = useFetch<any>(categoriesUrl);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>Error: {error}</h2>;
+  }
+
+  return (
+    <div>
+      <h1>Recipe Categories</h1>
+
+      {data?.categories?.map((category: any) => (
+        <p key={category.idCategory}>
+          {category.strCategory}
+        </p>
+      ))}
+    </div>
+  );
 }
 
 export default Home;
