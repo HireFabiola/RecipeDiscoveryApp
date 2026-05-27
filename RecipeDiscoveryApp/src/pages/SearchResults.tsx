f5c54bb5cea4f3744412a6c340b94bdbb9e484bf
Fabiola <fabiola.aurelien@gmail.com>
2026-05-27 16:58:32 -0400

    Added simple function to each page to check routes are functiopnal

--- RecipeDiscoveryApp/src/pages/SearchResults.tsx
+++ RecipeDiscoveryApp/src/pages/SearchResults.tsx
@@ -1,27 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-
-import Home from "../pages/Home";
-import Category from "../pages/Category";
-import RecipeDetail from "../pages/RecipeDetail";
-import Favorites from "../pages/Favorites";
-import SearchResults from "../pages/SearchResults";
-
-function App() {
-  return (
-
-    // Define the routes for the application
-    <Routes>
-      <Route path="/" element={<Home />} />
-
-      <Route path="/category/:categoryName" element={<Category />}/>
-
-      <Route path="/recipe/:recipeId" element={<RecipeDetail />}/>
-
-      <Route path="/favorites" element={<Favorites />}/>
-
-      <Route path="/search" element={<SearchResults />}/>
-    </Routes>
-  );
+function SearchResults() {
+  return <h1>Search Results Page</h1>;
 }
 
-export default App;
+export default SearchResults;
