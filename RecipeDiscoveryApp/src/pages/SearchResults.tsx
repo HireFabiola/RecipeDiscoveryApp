b2bfbc4e9d0afd30cc07839492c6be3d5ff72270
Fabiola <fabiola.aurelien@gmail.com>
2026-05-27 16:51:08 -0400

    Created Pages for the routes to land in the application

---
+++ RecipeDiscoveryApp/src/pages/SearchResults.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Category from "../pages/Category";
+import RecipeDetail from "../pages/RecipeDetail";
+import Favorites from "../pages/Favorites";
+import SearchResults from "../pages/SearchResults";
+
+function App() {
+  return (
+
+    // Define the routes for the application
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+      <Route path="/category/:categoryName" element={<Category />}/>
+
+      <Route path="/recipe/:recipeId" element={<RecipeDetail />}/>
+
+      <Route path="/favorites" element={<Favorites />}/>
+
+      <Route path="/search" element={<SearchResults />}/>
+    </Routes>
+  );
+}
+
+export default App;
